3b9a6b27bfff07b3421de9712ab74ee4f18e2f6c
Jisub Shim <jisub5322@naver.com>
2026-04-10 21:57:46 +0900

    소켓 프로그래밍 기초

--- Server_Rookiss/Server/Program.cs
+++ Server_Rookiss/Server/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Server
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
--- Server_Rookiss/ServerCore/Program.cs
+++ Server_Rookiss/ServerCore/Program.cs
@@ -1,10 +1,62 @@
-﻿namespace ServerCore
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // DNS(Domain Name System)
+            // www.jisub.com (Domain) -> 123.123.123.12 (IP)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            
+            // IP주소가 여러개 일 수 있음
+            // 구글같이 트래픽이 어마어마한 사이트는 사람마다 다른 주소를 알려주어 부하 분산
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 리슨 소켓
+            // AddressFamily : 네트워크 주소
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            
+            try
+            {
+                // 소켓 교육
+                listenSocket.Bind(endPoint);
+
+                // 시작
+                // backlog : 최대 대기 수 -> 나증에 라이브로 나갔을 때 조절하는 수치
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    // 손님 입장
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // 받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // 보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+                    clientSocket.Send(sendBuff);
+
+                    // 쫓아낸다
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
